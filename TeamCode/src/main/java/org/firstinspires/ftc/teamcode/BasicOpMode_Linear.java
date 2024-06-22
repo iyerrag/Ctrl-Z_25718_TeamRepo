e1d8dbb275f1381ac53b4875b6d93b3e40972c2f
Ragav Iyer <iyerrag829@gmail.com>
2024-06-22 17:30:26 -0400

    Merge remote-tracking branch 'origin/main'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Linear.java
@@ -75,10 +75,6 @@ public class BasicOpMode_Linear extends LinearOpMode {
         bR = hardwareMap.get(DcMotor.class, "BackRight");
 
 
-
-
-
-
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
