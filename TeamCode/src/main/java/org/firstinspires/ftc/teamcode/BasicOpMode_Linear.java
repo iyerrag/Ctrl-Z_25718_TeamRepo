070119fe06b725a76c756c0da7396ec7f1ed2f8c
Ragav Iyer <iyerrag829@gmail.com>
2024-07-06 15:49:51 -0400

    Merge remote-tracking branch 'origin/main'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Linear.java
@@ -92,6 +92,8 @@ public class BasicOpMode_Linear extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
+        double[] ab = {0, 0, 0, 0, 0, 0};
+
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
@@ -139,13 +141,25 @@ public class BasicOpMode_Linear extends LinearOpMode {
                // telemetry.addData("dTheta: ", "" + temp[2]);
             }
             else{
-                robot.waypointSettings(1, 1,  .0135,0.000, 0.0, 0.1, 0.33, .1, .1);
-                robot.toWaypoint(-120, 0, 30);
+                /*robot.waypointSettings(1, 1,  .0135,0.000, 0.0, 0.25, 0.1, 0.1, 0.33, .1, .1);
+                robot.toWaypoint(-120, 0, 45);
                 robot.toWaypoint(-60, 120, 0);
-                robot.toWaypoint(-120, 240, -30);
+                robot.toWaypoint(-120, 240, 15);
+                robot.toWaypoint(0, 240, -15);
+                robot.toWaypoint(-60, 120, -45);
+                robot.waypointSettings(.1, 1,  .0135,0.000, 0.0, 0.15, 0.1, 0.1, 0.33, .1, .1);
+                robot.toWaypoint(0, 0, 0);*/
+
+
+                robot.waypointSettings(1, 1,  0.027,0, 0.027 * 0.05, 0.1, 0.1, 0.1, .33, .1, .1);
                 robot.toWaypoint(0, 240, 0);
-                robot.toWaypoint(-60, 120, 30);
-                robot.toWaypoint(0, 0, 0);
+                /*robot.toWaypoint(-60, 0, -30);
+                robot.toWaypoint(-120, 240, 30);
+                robot.toWaypoint(-120, 0, -30);
+                robot.toWaypoint(-60, 240, 30);
+                //robot.waypointSettings(.1, 1,  .0135,0.000, 0.0, 0.15, 0.1, 0.1, .33, .1, .1);
+                robot.toWaypoint(0, 0, 0);*/
+
             }
 
             // Show the elapsed game time and wheel power.
@@ -156,7 +170,12 @@ public class BasicOpMode_Linear extends LinearOpMode {
             telemetry.addData("Y:", "" + Math.round(position[1] * 100.0) / 100.0);
             telemetry.addData("Theta:", "" + Math.round(position[2] * 100.0) / 100.0);
             telemetry.addData("Gyro Angle:", "" + gyroAngle);
-
+            /*telemetry.addData("globalCorrectionX: ", "" + ab[0]);
+            telemetry.addData("globalCorrectionY: ", "" + ab[1]);
+            telemetry.addData("correctionX: ", "" + ab[2]);
+            telemetry.addData("correctionY: ", "" + ab[3]);
+            telemetry.addData("correctionVectorMagnitude: ", "" + ab[4]);
+            telemetry.addData("correctionVectorAngle: ", "" + ab[5]);*/
 
             /*telemetry.addData("dx:", "" + differentials[0]);
             telemetry.addData("dy:", "" + differentials[1]);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chassis.java
@@ -32,7 +32,7 @@ public class chassis{
     static final double leftBias = 1.0;
     static private String angleMode;
 
-    private double waypointTargetX, waypointTargetY, waypointTargetTheta, waypointToleranceDist, waypointToleranceAng, waypointKp, waypointKi, waypointKd, waypointKc, waypointThetaWeight, waypointAccelLimXY, waypointAccelLimTheta;
+    private double waypointTargetX, waypointTargetY, waypointTargetTheta, waypointToleranceDist, waypointToleranceAng, waypointKp, waypointKi, waypointKd, waypointKcx, waypointKcy, waypointKctheta, waypointThetaWeight, waypointAccelLimXY, waypointAccelLimTheta;
 
 
     static final double maxA = 0.3;
@@ -150,13 +150,15 @@ public class chassis{
         localizer = new EulerianOdometry(x, y, theta, bL, bR, fL, imu, angleMode);
     }
 
-    public void waypointSettings(double toleranceDist, double toleranceAng, double Kp, double Ki, double Kd, double Kc, double thetaWeight, double accelLimXY, double accelLimTheta){
+    public void waypointSettings(double toleranceDist, double toleranceAng, double Kp, double Ki, double Kd, double Kcx, double Kcy, double Kctheta, double thetaWeight, double accelLimXY, double accelLimTheta){
         waypointToleranceDist = toleranceDist;
         waypointToleranceAng = toleranceAng;
         waypointKp = Kp;
         waypointKi = Ki;
         waypointKd = Kd;
-        waypointKc = Kc;
+        waypointKcx = Kcx;
+        waypointKcy = Kcy;
+        waypointKctheta = Kctheta;
         waypointThetaWeight = thetaWeight;
         waypointAccelLimXY = accelLimXY;
         waypointAccelLimTheta = accelLimTheta;
@@ -187,12 +189,16 @@ public class chassis{
         double previousErrX;
         double previousErrY;
         double previousErrTheta;
-        double Kcx = waypointKc;
-        double Kcy = waypointKc;
-        double Kctheta = waypointKc;
+        double Kcx = waypointKcx;
+        double Kcy = waypointKcy;
+        double Kctheta = waypointKctheta;
         double errX = 0;
         double errY = 0;
         double errTheta = 0;
+        double correctionVectorMagnitude = 0.0;
+        double correctionVectorAngle = 0.0;
+        double globalCorrectionX = 0.0;
+        double globalCorrectionY = 0.0;
 
 
         // Read current odometry position
@@ -228,38 +234,38 @@ public class chassis{
             Ptheta = errTheta;
 
             if(Px > 0){
-               Kcx = Math.abs(waypointKc);
+               Kcx = Math.abs(waypointKcx);
             }
             else if(Px == 0){
                 Kcx = 0;
             }
             else{
-                Kcx = -Math.abs(waypointKc);
+                Kcx = -Math.abs(waypointKcx);
             }
 
             if(Py > 0){
-                Kcy = Math.abs(waypointKc);
+                Kcy = Math.abs(waypointKcy);
             }
             else if(Py == 0){
                 Kcy = 0;
             }
             else{
-                Kcy = -Math.abs(waypointKc);
+                Kcy = -Math.abs(waypointKcy);
             }
 
             if(Ptheta > 0){
-                Kctheta = Math.abs(waypointKc);
+                Kctheta = Math.abs(waypointKctheta);
             }
             else if(Ptheta == 0){
                 Kctheta = 0;
             }
             else{
-                Kctheta = -Math.abs(waypointKc);
+                Kctheta = -Math.abs(waypointKctheta);
             }
 
-            Ix += errX * dt;
-            Iy += errY * dt;
-            Itheta += errTheta * dt;
+            Ix += dt * (errX + previousErrX) / 2.0;
+            Iy += dt * (errY + previousErrY) / 2.0;
+            Itheta += dt * (errTheta + previousErrTheta) / 2.0;
 
             Dx = (errX - previousErrX) / dt;
             Dy = (errY - previousErrY) / dt;
@@ -270,9 +276,12 @@ public class chassis{
             previousCorrectionY = correctionY;
             previousCorrectionTheta = correctionTheta;
 
-            correctionX =  (waypointKp * Px + waypointKi * Ix + waypointKd * Dx + Kcx);
+            correctionX = (waypointKp * Px + waypointKi * Ix + waypointKd * Dx + Kcx);
             correctionY = (waypointKp * Py + waypointKi * Iy + waypointKd * Dy + Kcy);
             correctionTheta = (waypointKp * Ptheta + waypointKi * Itheta + waypointKd * Dtheta + Kctheta);
+
+            globalCorrectionX = correctionX * Math.cos(currentTheta) - correctionY * Math.sin(currentTheta);
+            globalCorrectionY = correctionX * Math.sin(currentTheta) + correctionY * Math.cos(currentTheta);
 
             /*// Check if correction is within accelLim of previous correction to avoid slip
             if(!eqWT(correctionX, previousCorrectionX, accelLimXY)){
@@ -298,16 +307,51 @@ public class chassis{
                 else{
                     correctionTheta = previousCorrectionTheta - accelLimTheta;
                 }
-            }*/
+            }
 
             // Actuate Correction
-            double a = (correctionX+correctionY)*(Math.pow(2, -0.5));
-            double b = (-correctionX+correctionY)*(Math.pow(2, -0.5));
+            double a = (correctionX  + correctionY)*(Math.pow(2, -0.5));
+            double b = (-correctionX + correctionY)*(Math.pow(2, -0.5));
             fL.setPower(a * leftBias - correctionTheta * waypointThetaWeight);
             fR.setPower(b * rightBias + correctionTheta * waypointThetaWeight);
             bL.setPower(b * leftBias - correctionTheta * waypointThetaWeight);
             bR.setPower(a * rightBias + correctionTheta * waypointThetaWeight);
-            // *
+            */
+            // Actuate Correction
+            double a = (globalCorrectionX  + globalCorrectionY)*(Math.pow(2, -0.5));
+            double b = (-globalCorrectionX + globalCorrectionY)*(Math.pow(2, -0.5));
+            fL.setPower(a * leftBias - correctionTheta * waypointThetaWeight);
+            fR.setPower(b * rightBias + correctionTheta * waypointThetaWeight);
+            bL.setPower(b * leftBias - correctionTheta * waypointThetaWeight);
+            bR.setPower(a * rightBias + correctionTheta * waypointThetaWeight);
+
+            //return new double[]{globalCorrectionX, globalCorrectionY, correctionX, correctionY, correctionVectorMagnitude, correctionVectorAngle};
+        }
+        fL.setPower(0);
+        fR.setPower(0);
+        bL.setPower(0);
+        bR.setPower(0);
+    }
+
+    public void gyroTurn(double powLeft, double powRight, double targetAngle){
+        double[] currentAngle = imu.updateAngle();
+        if(targetAngle > currentAngle[0]){
+            while(currentAngle[0] < targetAngle){
+                fL.setPower(powLeft * leftBias);
+                bL.setPower(powLeft * leftBias);
+                fR.setPower(powRight * rightBias);
+                bR.setPower(powRight * rightBias);
+                localizer.updateOdometry();
+            }
+        }
+        else if(targetAngle < currentAngle[0]){
+            while(currentAngle[0] > targetAngle){
+                fL.setPower(powLeft * leftBias);
+                bL.setPower(powLeft * leftBias);
+                fR.setPower(powRight * rightBias);
+                bR.setPower(powRight * rightBias);
+                localizer.updateOdometry();
+            }
         }
         fL.setPower(0);
         fR.setPower(0);
